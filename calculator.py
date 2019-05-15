8599d9f8bb3e1b616529dd0927b547aded3f5bfa
Abdul Basit <abdul_basit313@outlook.com>
2019-05-16 02:19:36 +0500

    finished calculator

--- calculator.py
+++ calculator.py
@@ -1,13 +1,25 @@
-num1 = int(input('Type first number: '))
-num2 = int(input('Type second number: '))
-operator = input('Type operator: ')
+print('*' * 25) # for formation
 
+title = 'A simple python calculator'
+print(title)
+print('-' * 25) # for formation
+
+num1 = int(input('Type first number: '))
+operator = input('Type operator: ')
+num2 = int(input('Type second number: '))
+
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
 
 if operator == '+':
-   print(num1 + num2)
+   print(f'Result: {num1} + {num2} = {addition}')
 elif operator == '-':
-   print(num1 - num2)
+   print(f'Result: {num1} - {num2} = {subtraction}')
 elif operator == '*':
-   print(num1 * num2)
+   print(f'Result: {num1} * {num2} = {multiplication}')
 elif operator == '/':
-   print(num1 / num2)
+   print(f'Result: {num1} / {num2} = {division}')
+
+print('*' * 25) # for formation
