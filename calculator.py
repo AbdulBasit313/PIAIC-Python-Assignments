c60593f9e5673749e0da75647b50d79fcbf3311b
Abdul Basit <abdul_basit313@outlook.com>
2019-05-15 23:26:18 +0500

    done with basic calculator functionality

---
+++ calculator.py
@@ -0,0 +1,13 @@
+num1 = int(input('Type first number: '))
+num2 = int(input('Type second number: '))
+operator = input('Type operator: ')
+
+
+if operator == '+':
+   print(num1 + num2)
+elif operator == '-':
+   print(num1 - num2)
+elif operator == '*':
+   print(num1 * num2)
+elif operator == '/':
+   print(num1 / num2)
